0c33310f0334454cd024a720052badea4bdbd284
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-06-23 03:48:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your code here
+/*
+  Indicate that dessert is intended to be a private property by including an underscore (_) at the beginning.
+*/
+class Breakfast{
+  constructor(food, drink){
+    this.food = food;
+    this.drink = drink;
+  }
+}
+class Lunch{
+  constructor(salad, soup, drink){
+    this.salad = salad;
+    this.soup = soup;
+    this.drink = drink;
+  }
+}
+class Dinner{
+  constructor(salad, soup, entree, dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this._dessert = dessert;
+  }
+}
